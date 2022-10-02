2193cb7b4e56b5dd995f7b3a05732f9a3a763a23
repcomm <rccommandotac@gmail.com>
2022-10-01 21:21:49 -0500

    functional omniwheel drive math

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -2,10 +2,20 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.vuforia.Vec2F;
 
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.MotionDetection;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@TeleOp(name="Main OpMode", group="Main")
 public class Main extends OpMode {
   
   //variables for motors
@@ -15,13 +25,26 @@ public class Main extends OpMode {
   //variables for sensors
   BNO055IMU gyro = null;
   
+  Omnidrive drive;
+  DcMotor[] driveMotors;
   
   void init_motors () {
-    this.left_drive = this.hardwareMap.get(DcMotor.class, "left_drive");
-    this.right_drive = this.hardwareMap.get(DcMotor.class, "right_drive");
-  
-    this.left_drive.setDirection(DcMotor.Direction.REVERSE);
-    this.right_drive.setDirection(DcMotor.Direction.FORWARD);
+    int motorCount = 3;
+    
+    this.drive = new Omnidrive(motorCount);
+    this.driveMotors = new DcMotor[motorCount];
+    
+    for (int i=0; i<motorCount; i++) {
+      this.driveMotors[i] = this.hardwareMap.get(DcMotor.class, String.format("drive_%d", i) );
+    }
+    
+    this.telemetry.addData("Drive", "Angle radians %.2f, %.2f, %.2f",
+      this.drive.motorAnglesRadians[0],
+      this.drive.motorAnglesRadians[1],
+      this.drive.motorAnglesRadians[2]
+    );
+    this.telemetry.update();
+    
   }
   
   void init_sensors () {
@@ -42,8 +65,14 @@ public class Main extends OpMode {
     
   }
   
+  private Vec2 inputDirection;
+  private float inputMagnitude;
+  
   @Override
   public void init() {
+    
+    this.inputDirection = new Vec2();
+    this.inputMagnitude = 0;
     
     //say hello
     this.telemetry.addData("main", "Hello World");
@@ -54,30 +83,66 @@ public class Main extends OpMode {
     
     //init sensors we read from
     this.init_sensors();
-    
   }
+  
+  
   
   @Override
   public void loop() {
     
-    //check if A is pressed on gamepad 1
-    if (this.gamepad1.a) {
-      //log some text back so we can see
-      this.telemetry.addData("some caption here", "A is pressed, neat stuff..");
-      //notify system that we added that text and that it can log it for us
-      this.telemetry.update();
-    }
-  
     //get direction
     Orientation o = this.gyro.getAngularOrientation();
     
     //send orientation as text so we can see it
     this.telemetry.addData(
-        "Orientation", "{ x: %2.0f , y: %2.0f , z: %2.0f }",
-        o.firstAngle, o.secondAngle, o.thirdAngle
+        "Orientation", "{ x: %.2f, y: %.2f, z: %.2f }", + o.firstAngle, o.secondAngle, o.thirdAngle
     );
-    //update system to display text
+    
+    //copy stick direction
+    this.inputDirection.set(
+      this.gamepad1.left_stick_x,
+      this.gamepad1.left_stick_y
+    );
+    
+    //calculate magnitude of direction
+    this.inputMagnitude = this.inputDirection.magnitude();
+    
+    //get rid of magnitude from actual direction vector
+    this.inputDirection.normalize();
+    
+    float angleRadians = this.inputDirection.arctan2();
+    
+    this.drive.update(
+      angleRadians, this.inputMagnitude
+    );
+    
+    float turn = this.gamepad1.right_stick_x;
+    
+    this.telemetry.addData("Turn", "Value : %.2f", turn);
     this.telemetry.update();
     
+    float motorPower = 0;
+    
+    //apply drive calculations to physical motors
+    for (int i=0; i<this.drive.motorOutput.length; i++) {
+      motorPower = (this.drive.motorOutput[i] / 2) - (turn / 2);
+      
+      
+      this.driveMotors[i].setPower( motorPower );
+    }
+  
+    this.telemetry.addData("Drive output", "%.2f, %.2f, %.2f",
+      this.drive.motorOutput[0],
+      this.drive.motorOutput[1],
+      this.drive.motorOutput[2]
+    );
+    this.telemetry.update();
+  
+    
+    
+    //check if A is pressed on gamepad 1
+    if (this.gamepad1.a) {
+    
+    }
   }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omnidrive.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Omnidrive {
+  
+  public float[] motorAnglesRadians;
+  
+  public float[] motorOutput;
+  
+  private int motorCount;
+  
+  public static float DEG2RAD = (float)Math.PI / 180;
+  
+  /**Create a drive with a certain number of motors equally rotated around 360deg*/
+  public Omnidrive (int motorCount) {
+    this.motorCount = motorCount;
+    
+    //allocate space for angles of motors
+    this.motorAnglesRadians = new float[motorCount];
+    
+    //allocate space for motor torques that are calculated in 'update' function
+    this.motorOutput = new float[motorCount];
+    
+    //loop over every motor index
+    for (int i = 0; i<motorCount; i++) {
+      
+      //convert integers to float to preserve precision in math
+      float i_f = (float) i;
+      float motorCount_f = (float) motorCount;
+      
+      //between 0 and 1
+      float factor = i_f / motorCount_f;
+      
+      //degrees around a circle this motor is mounted
+      float degrees = factor * 360;
+      
+      //convert to radians
+      float radians = DEG2RAD * degrees;
+      
+      radians += DEG2RAD * 90;
+      
+      //set motor angle
+      this.setMotorAngleRadians(
+        i, radians
+      );
+    }
+  }
+  
+  public static float clampRadians (float theta) {
+    return theta %= (DEG2RAD * 360);
+  }
+  
+  public void setMotorAngleRadians(int index, float angleRadians) {
+    this.motorAnglesRadians[index] = angleRadians;// clampRadians(angleRadians);
+  }
+  
+  public static float motorFactor (float directionRadians, float motorRadians) {
+    float diff = directionRadians - motorRadians;
+    
+    return (float) Math.sin(diff);
+  }
+  
+  public void update (float directionRadians, float magnitude) {
+    for (int i=0; i<this.motorCount; i++) {
+      this.motorOutput[i] = motorFactor(directionRadians, this.motorAnglesRadians[i]) * magnitude;
+    }
+  }
+}
